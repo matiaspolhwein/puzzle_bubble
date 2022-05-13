76107bba4c03a76659ea90ca76eb270b949c5a67
matias polhwein <mrmatias892@gmail.com>
2022-05-13 18:38:26 -0400

    segundo commit script de movimiento de proyectil en progreso

---
+++ puzzle bubble/Assets/asets personales/scripts/cañon/Cañon_atributos.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cañon_atributos : MonoBehaviour
+{
+    Rigidbody2D rb2d;
+    public static float rotacion = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotacion = rb2d.rotation;
+    }
+}
--- puzzle bubble/Assets/asets personales/scripts/projectil/pro_movimiento.cs
+++ puzzle bubble/Assets/asets personales/scripts/projectil/pro_movimiento.cs
@@ -4,17 +4,43 @@ using UnityEngine;
 
 public class pro_movimiento : MonoBehaviour
 {
+
+    float x = 0.036f;
+    float y = 0.02f;
+    float r;
+
+    Vector2 sipar;
+    Vector2 nopar;
+    float x_par;
+    float y_par;
+ 
+    int vel = 5;
+    Rigidbody2D rb2d;
+
     // Start is called before the first frame update
     void Start()
     {
-        Rigidbody2D rb2d;
         rb2d = GetComponent<Rigidbody2D>();
-
+            
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        r = Cañon_atributos.rotacion;
+
+      
+        x_par = r/2;
+        y_par = r/2;
+
+        sipar = new Vector2(x_par, y_par);
+        nopar = new Vector2(sipar.x / 2 + sipar.x, sipar.y / 2 + sipar.y);
+
+        rb2d.MovePosition(sipar*vel*Time.deltaTime);
+        Debug.Log(x_par);
+
+      //  rb2d.position = rb2d.position + nopar * vel * Time.deltaTime;
+        {
+        }
     }
 }
